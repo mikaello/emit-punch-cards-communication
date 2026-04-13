52ab70048860f27ac47d03fa443df69889dfa650
mikaello <git@github.com:mikaello/emit-punch-cards-communication.git>
2026-04-13 21:58:24 +0200

    fix: use type imports/exports for type-only symbols to fix vite v8 (rolldown) build

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,28 @@
+import type { Ecard250 } from "./transform-stream-250";
 import {
-  Ecard250,
   EmitEkt250TransformStream,
   serialOptions250,
 } from "./transform-stream-250";
+import type { EcardMtr, MtrStatusMessage } from "./transform-stream-mtr4";
 import {
-  EcardMtr,
-  MtrStatusMessage,
   Mtr4TransformStream,
   serialOptionsMtr4,
 } from "./transform-stream-mtr4";
+import type { UsbFrame } from "./transform-stream-escan";
 import {
-  UsbFrame,
   EmitEscanUnpacker,
   EmitEscanTransformStream,
 } from "./transform-stream-escan";
 import { BatteryStatus, PackageType } from "./transform-stream-utils";
 
+export type { Ecard250, EcardMtr, MtrStatusMessage, UsbFrame };
 export {
   serialOptions250,
   serialOptionsMtr4,
-  Ecard250,
-  EcardMtr,
-  MtrStatusMessage,
   EmitEkt250TransformStream,
   Mtr4TransformStream,
   PackageType,
   BatteryStatus,
-  UsbFrame,
   EmitEscanUnpacker,
   EmitEscanTransformStream,
 };
