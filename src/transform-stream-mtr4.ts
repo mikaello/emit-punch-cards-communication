162510c170ecb9ca73d2a2ee9202a87257fc99a9
mikaello <git@github.com:mikaello/emit-punch-cards-communication.git>
2026-09-24 13:37:56 +0200

    chore: remove dormant MTR4 debug timer

--- src/transform-stream-mtr4.ts
+++ src/transform-stream-mtr4.ts
@@ -116,44 +116,11 @@ class Mtr4Unpacker {
   private readingFrame = false;
   onChunk: null | ((chunk: MtrStatusMessage | EcardMtr) => void);
 
-  consoleData: Uint8Array;
-  timeout: any | null;
-
   constructor() {
     this.onChunk = null;
-    this.consoleData = new Uint8Array(0);
-    this.timeout = null;
     this.data = new Uint8Array(ecardSize * 3);
     this.readPosition = 0;
     this.writePosition = 0;
-  }
-
-  /**
-   * @param uint8Array data to be appended to previous data, printed when data flow stops
-   */
-  printDataToConsole(uint8Array: Uint8Array) {
-    const newData = new Uint8Array(this.consoleData.length + uint8Array.length);
-    newData.set(this.consoleData, 0);
-    newData.set(uint8Array, this.consoleData.length);
-    this.consoleData = newData;
-
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-      this.timeout = null;
-    }
-
-    this.timeout = setTimeout(() => {
-      console.log(
-        "Number of bytes in this reading: " + this.consoleData.byteLength,
-      );
-      let allBytes = "";
-      for (let byteIdx in this.consoleData) {
-        allBytes += this.consoleData[byteIdx] + ",";
-      }
-      console.log(allBytes);
-
-      this.consoleData = new Uint8Array();
-    }, 5000);
   }
 
   parseEcard(ecardData: Uint8Array): EcardMtr {
