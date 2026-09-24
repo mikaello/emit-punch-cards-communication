18a5312ea5fdf06b19cade40c2d275b77b8d42e9
mikaello <2505178+mikaello@users.noreply.github.com>
2026-09-24 13:43:30 +0200

    feat: expose device commands and stream types (#465)
    Co-authored-by: mikaello <git@github.com:mikaello/emit-punch-cards-communication.git>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import type { Ecard250 } from "./transform-stream-250.js";
 import {
   EmitEkt250TransformStream,
+  OdTransformStream,
   serialOptions250,
 } from "./transform-stream-250.js";
-import type { EcardMtr, MtrStatusMessage } from "./transform-stream-mtr4.js";
+import type {
+  EcardMtr,
+  MtrStatusMessage,
+  MtrTypes,
+} from "./transform-stream-mtr4.js";
 import {
   Mtr4TransformStream,
   serialOptionsMtr4,
@@ -14,12 +19,17 @@ import {
   EmitEscanTransformStream,
 } from "./transform-stream-escan.js";
 import { BatteryStatus, PackageType } from "./transform-stream-utils.js";
+import * as escanCommands from "./escan-commands.js";
+import * as mtr4Commands from "./mtr4-commands.js";
 
-export type { Ecard250, EcardMtr, MtrStatusMessage, UsbFrame };
+export type { Ecard250, EcardMtr, MtrStatusMessage, MtrTypes, UsbFrame };
 export {
+  escanCommands,
+  mtr4Commands,
   serialOptions250,
   serialOptionsMtr4,
   EmitEkt250TransformStream,
+  OdTransformStream,
   Mtr4TransformStream,
   PackageType,
   BatteryStatus,
